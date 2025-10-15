855a5b02aa2483d892d2a0582530f746b687d441
icey-tea <eniolaakinpelumi11@gmail.com>
2025-10-15 14:23:43 -0500

    Intersection of Multiple arrays

--- Hashing/2248_Intersection_Multiple_Arrays.py
+++ Hashing/2248_Intersection_Multiple_Arrays.py
@@ -24,3 +24,20 @@ class Solution:
 # Then we are going to loop through the array and increment each intance of the number by 1
 # Then at the end all do is loop through our hash and check if the any number appears the same amount of times 
 #     as the len of the arr and just append it
+
+
+##Faster way to do this is. by using
+
+def intersection(self, nums: List[List[int]]) -> List[int]:
+        res = set(nums[0])
+        for i in range(1, len(nums)):
+            res &= set(nums[i])
+        res = list(res)
+        res.sort()
+        return res
+
+
+######
+The main part her is '&=' which is intersection
+so for this all we are doing is creating a set of the first list and using intersection '&=' to 
+    find the commonalities(intersection) in the other lists 
